7aac410924a33aab086a4f6fd6d1ceeca2a720ae
faelv <rafael_alt_dev@outlook.com>
2020-02-24 18:31:40 -0300

    Adds more tests

--- test/suite/extension.test.js
+++ test/suite/extension.test.js
@@ -3,7 +3,23 @@ const path = require('path');
 const fs = require('fs');
 const simple = require('simple-mock');
 const vscode = require('vscode');
-const { ComposerExtension } = require('../../src/extension');
+//const { ComposerExtension } = require('../../src/extension');
+
+function cleanDir(dir, rootDir = true) {
+	fs.readdirSync(dir).forEach((file) => {
+		const cur = path.join(dir, file)
+		if (fs.lstatSync(cur).isDirectory()) {
+			cleanDir(cur, false)
+		} else {
+			if (file !== 'README.md') {
+				fs.unlinkSync(cur)
+			}
+		}
+	})
+	if (!rootDir) {
+		fs.rmdirSync(dir)
+	}
+}
 
 suite('Commands', () => {
 
@@ -15,24 +31,28 @@ suite('Commands', () => {
 	let quickPickIndex = 0
 
 	suiteSetup(() => {
-		testDir = path.resolve(__dirname, '..', '..', '.vscode-test', 'test-data')
-		if (!fs.existsSync(testDir)) {
-			fs.mkdirSync(testDir)
-		}
-		testDir = fs.mkdtempSync(`${testDir}${path.sep}`)
+		testDir = path.resolve(__dirname, '..', '..', 'test', 'data')
+		cleanDir(testDir)
 
 		simple.mock(vscode.window, 'showQuickPick').callFn(async (items, options) => items[quickPickIndex])
 		simple.mock(vscode.window, 'showOpenDialog').callFn(async (options) => {
 			return [vscode.Uri.file(testDir)]
 		})
 
-		extension = new ComposerExtension()
-		simple.mock(extension.commands, 'addWorkspaceFolder').returnWith(undefined)
+		//extension = new ComposerExtension()
+
+		/* simple.mock(extension.commands, 'addWorkspaceFolder').callFn((folderUri) => {
+			vscode.workspace.updateWorkspaceFolders(
+        vscode.workspace.workspaceFolders ? vscode.workspace.workspaceFolders.length : 0,
+        0,
+        {uri: folderUri}
+      )
+		}) */
 	})
 
 	suiteTeardown(() => {
 		simple.restore()
-		//fs.rmdirSync(testDir, {recursive: true})
+		cleanDir(testDir)
 	})
 
 	test('init', async () => {
@@ -57,13 +77,24 @@ suite('Commands', () => {
 			eDisp.dispose()
 		})
 
-		await extension.commands.commandInit()
+		//await extension.commands.commandInit()
+		await vscode.commands.executeCommand('composerCompanion.init')
 		if (terminal) {
 			terminal.dispose()
 		}
-		assert.ok(fs.existsSync(path.join(testDir, 'composer.json')))
+		assert.ok(fs.existsSync(path.join(testDir, 'composer.json')), 'Failed to init composer.json')
 	});
 
+	test('open', async () => {
+		const fileUri = vscode.Uri.file(path.join(testDir, 'composer.json'))
 
+		//const editorA = await extension.commands.commandOpen(fileUri)
+		const editorA = await vscode.commands.executeCommand('composerCompanion.open', fileUri)
+		assert.notEqual(editorA, undefined, 'Failed to open composer.json from parameter')
+
+		//const editorB = await extension.commands.commandOpen()
+		const editorB = await vscode.commands.executeCommand('composerCompanion.open')
+		assert.notEqual(editorB, undefined, 'Failed to open composer.json from picked folder')
+	});
 
 });
