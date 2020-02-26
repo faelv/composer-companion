6f9f99f671c473d98470978a61b9ddbe1edf3092
faelv <rafael_alt_dev@outlook.com>
2020-02-26 12:25:11 -0300

    Fixes running tests from CLI

--- test/runTest.js
+++ test/runTest.js
@@ -16,7 +16,7 @@ async function main() {
 		await runTests({
 			extensionDevelopmentPath,
 			extensionTestsPath,
-			launchArgs: ['--disable-extensions']
+			launchArgs: [path.join(__dirname, 'data'), '--disable-extensions']
 		});
 	} catch (err) {
 		console.error('Failed to run tests');
